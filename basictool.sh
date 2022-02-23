ea8e9a6071b07d2df3d0bef7395201e0124cc0ad
AnkitSimbiotiktech <87964141+AnkitSimbiotiktech@users.noreply.github.com>
2022-02-23 12:43:03 +0530

    1st

---
+++ basictool.sh
@@ -0,0 +1,45 @@
+#!/bin/bash/
+
+# Jenkins Install 
+sudo apt install openjdk-8-jdk 
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+#Helm install 
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm -y 
+
+# Integrating ACR witk AKS
+az account set --subscription subscription-id
+az aks update -n aksclustername -g rgname --attach-acr acrnaeme
+
+# Install Kubectl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin/kubectl
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
+
+# Install az
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install Docker on Ubuntu
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y 
+sudo systemctl enable docker
+sudo systemctl status docker 
